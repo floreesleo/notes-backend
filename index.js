998445d93c352d368abd676f6721d3d4a3c5849e
Leonardo Flores <florees.leo@gmail.com>
2024-12-03 14:44:21 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(cors());
+app.use(morgan("tiny"));
+app.use(express.json());
+app.use(express.static("dist"));
+
+console.log("Hello");
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+];
+
+app.get("/", (request, response) => {
+  response.send("<h1>Home Page</h1>");
+});
+
+app.get("/api/notes", (request, response) => {
+  response.json(notes);
+});
+
+app.get("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find((note) => note.id === id);
+
+  if (!note) {
+    response.status(404).send("<h1>Note Not Found</h1>").end();
+  }
+
+  response.json(note);
+});
+
+app.delete("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+
+  const newNotes = notes.filter((note) => note.id !== id);
+  // response.status(204).end();
+  response.json(newNotes).status(204).end();
+});
+
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateId(),
+  };
+
+  notes = notes.concat(note);
+
+  response.json(note);
+});
+
+app.listen(PORT, () => {
+  console.log(`Express server is running on PORT: http://localhost:${PORT}`);
+});
